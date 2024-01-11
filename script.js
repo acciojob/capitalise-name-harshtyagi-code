242e2457a42b91ca4387853ef607742cdb754cdc
Harsh Tyagi <69513043+harshtyagi-code@users.noreply.github.com>
2024-01-11 13:39:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const input=document.getElementById("fname");
+input.addEventListener('focusout', ()=>{
+	let str=input.value;
+	str=str.toUpperCase();
+	input.value=str;
+});
